c7d1c9f76854cb168d787ee329f2a8d855dd816a
riku <riku.vaatanen@gmail.com>
2025-09-02 20:01:27 +0300

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // src/App.tsx
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <main className="flex-1">
         <Hero />
+        <About />
       </main>
     </div>
   );
