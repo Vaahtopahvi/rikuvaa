e08c008a0c9c41dadbb2bdcd8622c952ba130199
riku <kermakahvi@hotmail.com>
2025-09-15 02:58:45 +0300

    added predictions for the 8ball

---
+++ src/data/predictions.ts
@@ -0,0 +1,12 @@
+export const predictions = [
+  "All Paths Lead To Yes",
+  "You May Rely On It",
+  "Most Likely",
+  "Yes, Very Soon",
+  "Outlook Good",
+  "After My Delicious Lunch",
+  "He Seems Like A Good Fit To Our Team",
+  "It Is Certain",
+  "There's A Good Chance",
+  "Already On It",
+];
